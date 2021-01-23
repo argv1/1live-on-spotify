9ddbfde9439ee227f1559dee191f367c6e713033
argv1 <christian.meyer@hofa.biz>
2021-01-23 02:32:57 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+'''
+    Spotify playlist generator based on the tracks from einslive Klaus Fiehes "Korrektes Zeug"
+    Created by argv1 https://github.com/argv1/1live-on-spotify
+'''
+import argparse
+import csv
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import json
+import pandas as pd
+from   pathlib import Path
+import re
+import requests
+import spotipy
+import spotipy.util as util
+
+# Define path and filename
+base_path = Path(__file__).parent.absolute()
+config_f = base_path / "config.ini"
+playlists = base_path / "playlists"
+spotify_username = "YOUR-USER-ID"
+SPOTIPY_CLIENT_ID = "YOUR_SPOTIPY_CLIENT_ID"
+SPOTIPY_CLIENT_SECRET = "YOUR_SPOTIPY_CLIENT_SECRET"
+spotify_scope = "playlist-modify-public" 
+token = util.prompt_for_user_token(username=spotify_username, scope=spotify_scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri='http://localhost/')
+sp = spotipy.Spotify(auth=token)
+playlist_description = f"Created by argv1 https://github.com/argv1/1live-on-spotify"
+
+def get_track_ids(df):
+    # Get Spotify track ids for the playlist
+    track_ids, track_ids_not_found = [], []
+    # Track Info Box Flow
+    for i in range(len(df)):
+        results = sp.search(q=f"{df['Titel'][i]} {df['Interpret'][i]} ", limit=5, type='track')
+        # if track isn't on spotify as queried, go to next track
+        if results['tracks']['total'] == 0: 
+            continue
+        else:
+            for j in range(len(results['tracks']['items'])):
+                # Get right response by matching on artist and title
+                if fuzz.partial_ratio(results['tracks']['items'][j]['artists'][0]['name'], df['Interpret'][i]) > 90 and fuzz.partial_ratio(results['tracks']['items'][j]['name'], df['Titel'][i]) > 90 : 
+                    track_ids.append(results['tracks']['items'][j]['id']) 
+                    break
+                else:
+                    track_ids_not_found.append((f"{df['Titel'][i]} from {df['Interpret'][i]}"))
+                    continue
+    print(f"Thw following tracks could not be found on spotify: {set(track_ids_not_found)}")
+    return track_ids
+
+def get_playlist_id(playlist_name):
+    playlists = sp.user_playlists(spotify_username)
+    # iterate through spotify_username playlists
+    for playlist in playlists['items']:
+        # filter for newly created playlist
+        if playlist['name'] == playlist_name:  
+            return(playlist['id'])
+
+def remove_duplicates(seq, idfun=None):
+   # Thx to https://www.peterbe.com/plog/uniqifiers-benchmark
+   if idfun is None:
+       def idfun(x): return x
+   seen = {}
+   result = []
+   for item in seq:
+       marker = idfun(item)
+       if marker in seen: continue
+       seen[marker] = 1
+       result.append(item)
+   return result
+
+def generate_playlist(df, date): 
+    # Create playlist
+    playlist_name = f"Klaus Fiehes Korrektes Zeug vom {date}"    
+    sp.user_playlist_create(user=spotify_username, name=playlist_name, description=playlist_description)
+
+    # Get Tracks
+    track_ids = get_track_ids(df)
+
+    # Remove duplicate tracks order preserving  
+    track_ids = remove_duplicates(track_ids)
+    playlist_id = get_playlist_id(playlist_name)
+
+    # Populate playlist
+    sp.user_playlist_add_tracks(spotify_username, playlist_id, track_ids)
+
+def get_tracks(url):
+    try:
+        content = requests.get(f"{url}")
+        date = re.findall(r'<table class="thleft"><caption>Playlist der Sendung vom (\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*)', content.text)
+        filename = f"{date[0][3]}-{date[0][2]}-{date[0][1]}"
+
+        tab_list = pd.read_html(content.content)
+        df = tab_list[0]
+        df.dropna(axis=0, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        df.to_csv(f"{playlists}/{filename}.csv", index=False, sep=";", encoding="iso-8859-1")
+    except:
+        print(f"{url} does not exists.")
+    return(df, filename)
+
+def main():
+    # Initiate the parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', help='Enter the url of the show that should be scrapped', type=str, required=True)
+    args = parser.parse_args()
+    df, date = get_tracks(args.url)
+    generate_playlist(df, date)
+        
+if __name__  == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -1,4 +1,30 @@
 ## 1live-on-spotify
 ======================
  
-under construction
+## Purpose
+Create a spotify playlist based on the songs from Klaus Fiehes show Korrektes Zeug using spotipy.
+
+## Playlists
+If you just want to listen to the songs, you could follow me [spotify profile](https://open.spotify.com/user/11123260766) or just visit my [spotify playlists](https://open.spotify.com/playlist/2OS7vZcECoJvIHkp6yedFz).
+
+## Usage
+run pip to ensure all requirements are fulfilled
+ 
+```bash
+pip3 install -r requirements.txt
+```
+</br>
+now you can run the script by providing a new url:
+```bash
+i.e. main.py -u https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/index.html
+```
+
+## License
+This code is licensed under the [GNU General Public License v3.0](https://choosealicense.com/licenses/gpl-3.0/). 
+For more details, please take a look at the [LICENSE file](https://github.com/argv1/OReilly-Downloader/blob/master/LICENSE).
+
+## Outlook
+[x] Scrap playlist from 1live.de
+[x] Create playlist on spotify based on scraped tracks
+[] Improving unicode & character encoding
+Feel free to adjust the code
