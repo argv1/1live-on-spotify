2341e597ede2e867043660cea735c0f03c34a1a0
argv1 <christian.meyer@hofa.biz>
2021-01-27 23:49:35 +0100

    Update helper3.py

--- helper/helper3.py
+++ helper/helper3.py
@@ -71,22 +71,6 @@ def generate_playlist(df, date):
     # Populate playlist
     sp.user_playlist_add_tracks(SPOTIFY_USERNAME, playlist_id, track_ids)
 
-def get_tracks(page):
-    try:
-        content = requests.get(f"{base_url}{page}.html")
-        date = re.findall(r'<table class="thleft"><caption>Playlist der Sendung vom (\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*)', content.text)
-        filename = f"{date[0][3]}-{date[0][2]}-{date[0][1]}"
-
-        tab_list = pd.read_html(content.content)
-        df = tab_list[0]
-        df.dropna(axis=0, inplace=True)
-        df.reset_index(drop=True, inplace=True)
-        df.to_csv(f"{playlists}/{filename}.csv", index=False, sep=";", encoding="iso-8859-1")
-    except:
-        print(f"{page} does not exists.")
-        return('test', 'test', False)
-    return(df, filename, True)
-
 def main():
     # Initiate the parser
     parser = argparse.ArgumentParser()
