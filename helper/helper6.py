7c77a115c5a514fbc2779b206c72d6490da4de1e
argv1 <christian.meyer@hofa.biz>
2021-06-13 11:15:40 +0200

    Add files via upload

---
+++ helper/helper6.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+    Spotify playlist mass renaming
+
+'''
+import spotipy
+import spotipy.util as util
+
+# Define path and filename
+SPOTIFY_USERNAME = "SPOTIFY_USERNAME"
+SPOTIFY_CLIENT_ID = "SPOTIFY_CLIENT_ID"
+SPOTIFY_CLIENT_SECRET = "SPOTIFY_CLIENT_SECRET"
+spotify_scope = "playlist-modify-public" 
+token = util.prompt_for_user_token(username=SPOTIFY_USERNAME, scope=spotify_scope, client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri='http://localhost:8888/callback')
+sp = spotipy.Spotify(auth=token)
+playlist_description = f"Klaus Fiehes weekly 1live show, formaly known as Raum und Zeit\nCreated by argv1\nhttps://github.com/argv1/1live-on-spotify" 
+playlists = sp.user_playlists(SPOTIFY_USERNAME)
+
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        if("1live Fliehe" in playlist['name']):
+            playlist_name = "1live F" + playlist['name'][8:]
+            sp.user_playlist_change_details(sp.user("11123260766"), playlist['id'], name=playlist_name, public=True, collaborative=None, description=playlist_description)    
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
