35794327909158812f8cd44f9a1186f70fadfa81
argv1 <github@hofa.biz>
2022-12-28 20:05:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,9 @@
 '''
 import argparse
 from configparser import ConfigParser
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+from fuzzywuzzy import fuzz, process
 import pandas as pd
-from   pathlib import Path
+from pathlib import Path
 import re
 import requests
 import spotipy
@@ -32,18 +31,18 @@ def get_track_ids(df):
     track_ids, track_ids_not_found = [], []
     # Track Info Box Flow
     for i in range(len(df)):
-        results = sp.search(q=f"{df['Titel'][i]} {df['Interpret'][i]} ", limit=5, type='track')
+        results = sp.search(q=f"{df['Artist'][i]} {df['Track'][i]} ", limit=5, type='track')
         # if track isn't on spotify as queried, go to next track
         if results['tracks']['total'] == 0: 
             continue
         else:
             for j in range(len(results['tracks']['items'])):
                 # Get right response by matching on artist and title
-                if fuzz.partial_ratio(results['tracks']['items'][j]['artists'][0]['name'], df['Interpret'][i]) > 90 and fuzz.partial_ratio(results['tracks']['items'][j]['name'], df['Titel'][i]) > 90 : 
+                if fuzz.partial_ratio(results['tracks']['items'][j]['artists'][0]['name'], df['Artist'][i]) > 90 and fuzz.partial_ratio(results['tracks']['items'][j]['name'], df['Track'][i]) > 90 : 
                     track_ids.append(results['tracks']['items'][j]['id']) 
                     break
                 else:
-                    track_ids_not_found.append((f"{df['Titel'][i]} from {df['Interpret'][i]}"))
+                    track_ids_not_found.append((f"{df['Track'][i]} from {df['Artist'][i]}"))
                     continue
     print(f"The following tracks could not be found on spotify: {set(track_ids_not_found)}")
     return track_ids
@@ -58,7 +57,7 @@ def get_playlist_id(playlist_name):
 
 def generate_playlist(df, date): 
     # Create playlist
-    playlist_name = f"1live Fliehe vom {date}"    
+    playlist_name = f"1live Klaus Fliehe vom {date}"
     sp.user_playlist_create(user=SPOTIFY_USERNAME, name=playlist_name, description=playlist_description)
 
     # Get Tracks
@@ -69,30 +68,32 @@ def generate_playlist(df, date):
     playlist_id = get_playlist_id(playlist_name)
 
     # Populate playlist
-    sp.user_playlist_add_tracks(SPOTIFY_USERNAME, playlist_id, track_ids)
+    sp.playlist_add_items(playlist_id, track_ids)
 
 def get_tracks(url):
     try:
         content = requests.get(f"{url}")
-        date = re.findall(r'<table class="thleft"><caption>Playlist der Sendung vom (\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*)', content.text)
-        filename = f"{date[0][3]}-{date[0][2]}-{date[0][1]}"
-
-        tab_list = pd.read_html(content.content)
-        df = tab_list[0]
-        df.dropna(axis=0, inplace=True)
-        df.reset_index(drop=True, inplace=True)
-        try:
-            df.to_csv(f"{playlists}/{filename}.csv", index=False, sep=";", encoding="cp1252")
-        except: 
-            df.to_csv(f"{playlists}/{filename}.csv", index=False, sep=";", encoding="utf-8")    
     except:
         print(f"{url} does not exists.")
+
+    date = re.findall(r'<table class="thleft"><caption>1LIVE Fiehe - Playlist der Sendung vom (\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*)',content.text)
+    filename = f"{date[0][3]}-{date[0][2]}-{date[0][1]}"
+
+    tab_list = pd.read_html(content.content)
+    df = tab_list[0]
+    df.dropna(axis=0, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    try:
+        df.to_csv(f"{playlists}/{filename}.csv", index=False, sep=";", encoding="cp1252")
+    except:
+        df.to_csv(f"{playlists}/{filename}.csv", index=False, sep=";", encoding="utf-8")
+
     return(df, filename)
 
 def main():
     # Initiate the parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--url', help='Enter the url of the show that should be scrapped', type=str, default="https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/index.html")
+    parser.add_argument('-u', '--url', help='Enter the url of the show that should be scrapped', type=str, default="https://www1.wdr.de/radio/1live/on-air/sendungen/1live-fiehe/")
     args = parser.parse_args()
     df, date = get_tracks(args.url)
 
